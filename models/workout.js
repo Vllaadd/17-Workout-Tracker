3d74fe16b8ef06d9e2cdcc236ca3b0875ba49a4f
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-03 11:55:12 -0500

    workout model created

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+const { Schema } = require("mongoose")
+
+const = require('mongoose')
+
+module.exports = model('workout', new Schema({
+    day: {
+        type: Date,
+        required: true
+    },
+    exercises: [{
+        type:{type: String},
+        name:{type: String},
+        duration: {type: Number},
+        weight: {type: Number},
+        reps: {type: Number},
+        sets: {type: Number},
+        distance: {type: Number}
+    }]
+}))
