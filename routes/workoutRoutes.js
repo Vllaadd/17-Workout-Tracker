cbbd0c9af8ac00bd2ee69b71ba1a5fc63e9e524b
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-04 05:26:22 -0500

    create a new workout route

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,9 +2,16 @@ const router = require('express').Router()
 const workout = require('../models/workout')
 
 
-// GET all workouts
+//showing previously logged workouts
 router.get('/workouts', (req, res) => {
     workout.find()
     .then(workouts => res.json(workouts))
+    .catch(e => console.error(e))
+})
+
+//creating a new workout
+router.post('/workouts', (req, res) => {
+    workout.create({day: new Date()})
+    .then((data) => res.json(data))
     .catch(e => console.error(e))
 })
