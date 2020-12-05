9baada7b370098bfe158526dc34257d581e11ff9
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-05 10:54:30 -0500

    server.js code cleaned

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+//===IMPORT NPM PACKAGES====================================
 const express = require('express')
 const = require('path')
 const app = express()
+const mongoose = require('mongoose')
 
+//===SERVING STATIC FILES===================================
 app.use(express.static(join(__dirname, 'public')))
+
+//===DATA PARSING===========================================
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+//===DEPLOYING ON HEROKU====================================
+mongoose.connect(process.env.MONGODB_URL || 'mongodb: //localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongodb+srv://Vlad:columbia20mongo@cluster0.xm9q3.mongodb.net/testing?retryWrites=true&w=majorityz
+
+//===IMPORTING ROUTES======================================
 app.use(require('./routes'))
   
   require('./config')
