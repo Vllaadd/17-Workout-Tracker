93a7d5dd99c0cc386d97a5ba4656b8e844893b5e
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-06 18:24:18 -0500

    listening server code was wrong

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path')
 const app = express()
 const mongoose = require('mongoose')
 
+//===SETTING UP THE PORT=================================
+const PORT = process.env.PORT || 3000;
+
 //===STATIC DIRECTORY===================================
 app.use(express.static('public'));
 
@@ -13,14 +16,17 @@ app.use(express.json())
 
 //===DEPLOYING ON HEROKU====================================
 mongoose.connect(process.env.MONGODB_URL || 'mongodb: //localhost/workout', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 //===IMPORTING ROUTES======================================
 app.use(require('./routes/htmlRoutes'))
 app.use(require('./routes/workoutRoutes'))
   
-  require('./config')
-    .then(() => app.listen(process.env.PORT || 3000))
-    .catch(e => console.error(e))
+
+app.listen(PORT, () =>{
+  console.log(`App listening on http://localhost:${PORT}`)
+})
