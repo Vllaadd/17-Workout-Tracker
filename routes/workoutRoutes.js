e58b4cc01ed36b513217c5c6eb4eedfb0b801083
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-07 05:11:03 -0500

    typo workout fixed to Workout

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router()
-const workout = require('../models/workout')
+const Workout = require('../models')
 
 //===showing previously logged workouts=======================
 router.get('/workouts', (req, res) => {
-    workout.find()
+    Workout.find()
     .then(workouts => res.json(workouts))
     .catch(e => console.error(e))
 })
 
 //===creating a new workout===================================
 router.post('/workouts', (req, res) => {
-    workout.create({day: new Date()})
+    Workout.create({day: new Date()})
     .then((data) => res.json(data))
     .catch(e => console.error(e))
 })
